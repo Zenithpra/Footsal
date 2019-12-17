fdd0a75cb6a1d1ab4f542fba8355c242648bb8e3
Zenithpra <49400681+Zenithpra@users.noreply.github.com>
2019-12-17 13:59:24 +0545

    ..

--- app/src/main/java/com/zenith/footsal/MainActivity.java
+++ app/src/main/java/com/zenith/footsal/MainActivity.java
@@ -3,12 +3,34 @@ package com.zenith.footsal;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import static com.zenith.footsal.R.id.btnlogin;
 
 public class MainActivity extends AppCompatActivity {
 
+    ImageView ivlogo;
+    TextView tvuser, tvpass;
+    Button btnlogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ivlogo =findViewById(R.id.ivlogo);
+        tvuser =findViewById(R.id.tvuser);
+        tvpass = findViewById(R.id.tvpass);
+        btnlogin = findViewById(R.id.btnlogin);
+
+        btnlogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
     }
 }
